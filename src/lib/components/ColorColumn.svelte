020ff5a78a3f157b3335dade46c8acb4ce2b1a6b
md-y <15069105+md-y@users.noreply.github.com>
2023-06-20 10:56:15 +0200

    Rework noise

--- src/lib/components/ColorColumn.svelte
+++ src/lib/components/ColorColumn.svelte
@@ -1,17 +1,11 @@
 <script lang="ts">
   import type { TransitionConfig } from "svelte/types/runtime/transition";
   import { expoInOut } from "svelte/easing";
-  import { v4 as uuid } from "@lukeed/uuid";
-  import NoiseFilter from "./NoiseFilter.svelte";
 
   export let color: string;
-  export let noiseColor = color;
   export let noiseOpacity = 1;
   export let size = 1;
   export let order = 0;
-
-  const filterId = `filter-${uuid()}`;
-  const filterSeed = parseInt(color.replace("#", ""), 16) || 1;
 
   const transitionDuration = 1000;
 
@@ -19,22 +13,22 @@
     return {
       duration: transitionDuration,
       delay: 0,
-      css: (t: number) => `flex-grow: ${expoInOut(t) * size};`,
+      css: (t: number) =>
+        `flex: ${expoInOut(t) * size} ${expoInOut(t) * size};`,
     };
   }
 </script>
 
 <div
   class="color-col"
+  class:noise={noiseOpacity > 0}
   in:flexTransition
   out:flexTransition
   style:--column-color={color}
-  style:--filter-url="url(#{filterId})"
   style:--noise-opacity={noiseOpacity}
   style:--order={order}
-  style:--flex-grow={size}
+  style:--flex-size={size}
 >
-  <NoiseFilter id={filterId} color={noiseColor} seed={filterSeed} />
   <slot />
 </div>
 
@@ -42,19 +36,16 @@
   @import "global.scss";
 
   .color-col {
-    flex: var(--flex-grow) var(--flex-grow);
+    flex: var(--flex-size) var(--flex-size);
+    will-change: flex;
     order: var(--order);
-    transform: skewY($global-tilt-angle);
     background-color: var(--column-color);
     overflow: hidden;
 
-    &::before {
-      content: "";
-      filter: var(--filter-url);
-      position: absolute;
-      width: 100%;
-      height: 100%;
-      opacity: var(--noise-opacity);
+    @include skew();
+
+    &.noise::before {
+      @include noise-background(var(--noise-opacity));
     }
 
     &::after {

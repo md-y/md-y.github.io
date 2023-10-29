f117105d86aa8516292496b1ed664025ec1215fb
md-y <15069105+md-y@users.noreply.github.com>
2023-10-29 17:34:17 -0500

    In progress portfolio page

---
+++ src/lib/components/PortfolioItem.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import Img from "@zerodevx/svelte-img";
+  import RememberMeImg from "$assets/portfolio/rememberme.png?run";
+</script>
+
+<div class="portfolio-item">
+  <div class="photo">
+    <Img src={RememberMeImg} />
+  </div>
+  <div class="text">
+    <h1>RememberMe</h1>
+    <h2>Study app for remembering people</h2>
+  </div>
+</div>
+
+<style lang="scss">
+  @import "global.scss";
+
+  .portfolio-item {
+    display: flex;
+    flex-direction: row;
+    gap: $base-margin;
+
+    @include unskew-rotate();
+    margin-left: $base-margin;
+
+    .photo {
+      position: relative;
+
+      :global(img) {
+        height: 100px;
+        width: auto;
+        object-fit: cover;
+      }
+    }
+
+    .text {
+      display: flex;
+      flex-direction: column;
+
+      h1,
+      h2 {
+        font-family: "Roboto", sans-serif;
+        color: $global-dark-text;
+        margin: 0;
+      }
+    }
+  }
+</style>
--- src/routes/portfolio/+page.svelte
+++ src/routes/portfolio/+page.svelte
@@ -1,20 +1,37 @@
 <script lang="ts">
+  import BackButton from "$lib/components/BackButton.svelte";
   import ColorColumn from "$lib/components/ColorColumn.svelte";
+  import PortfolioItem from "$lib/components/PortfolioItem.svelte";
+
+  import RememberMeImg from "$assets/portfolio/rememberme.png?run";
 </script>
 
-<ColorColumn color="#242038" noiseOpacity={0.4} order={11}>
+<ColorColumn color="#242038" noiseOpacity={0.4} size={2} order={11}>
+  <BackButton />
   <div id="title-container">
-    <h1>Hi, I'm Sam</h1>
+    <h1>Portfolio</h1>
   </div>
 </ColorColumn>
-<ColorColumn color="#f7ece1" noiseOpacity={1} size={3} order={12} />
-<ColorColumn color="#242038" noiseOpacity={0.4} order={13} />
+<ColorColumn color="#f7ece1" noiseOpacity={1} order={12}>
+  <PortfolioItem />
+</ColorColumn>
+<ColorColumn color="#cac4ce" noiseOpacity={0.9} order={13}>
+  <PortfolioItem />
+</ColorColumn>
+<ColorColumn color="#8d86c9" noiseOpacity={0.5} order={14}>
+  <PortfolioItem />
+</ColorColumn>
+<ColorColumn color="#725ac1" noiseOpacity={0.4} order={15}>
+  <PortfolioItem />
+</ColorColumn>
+<ColorColumn color="#8d86c9" noiseOpacity={0.5} order={16}>
+  <PortfolioItem />
+</ColorColumn>
+<ColorColumn color="#242038" noiseOpacity={0.3} size={2} order={17} />
 
 <style lang="scss">
   @use "sass:math";
   @import "global.scss";
-
-  $left-margin: 2rem;
 
   #title-container {
     display: flex;
@@ -29,7 +46,7 @@
       font-family: "Inter", sans-serif;
       font-weight: 600;
       font-size: 12vmin;
-      margin-left: $left-margin;
+      margin-left: $base-margin;
     }
   }
 </style>
